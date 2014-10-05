80411a2f9807fbe7c7d27e41b69f963b02f2e6ff
Brian Downs <brian.downs@gmail.com>
2014-10-05 10:53:25 -0700

    update nmap mod to use new config

--- enumerator/lib/nmap.py
+++ enumerator/lib/nmap.py
@@ -21,6 +21,7 @@ from blinker import signal
 
 import delegator
 from .process_manager import ProcessManager
+from config import Config as cnf
 
 PROCESSES = [{
     'command': 'nmap -Pn %(scan_mode)s -sS -oN %(output_dir)s/%(host)s-tcp-standard.txt -oG %(output_dir)s/%(host)s-tcp-greppable.txt %(host)s',
